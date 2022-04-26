977af8cf59f3660aae8f4e1146a5372443602d15
EamonYin <xiaominglovejava@163.com>
2022-04-26 16:59:05 +0800

    lambda 默认方法练习代码

---
+++ src/main/java/com/eamon/java8/java/example/newInterfaceMethod/newMethodTest.java
@@ -0,0 +1,34 @@
+package com.eamon.java8.java.example.newInterfaceMethod;
+
+import org.junit.Test;
+
+/**
+ * @Author: Eamon
+ * @Date: 2022/4/26 16:50
+ */
+public class newMethodTest {
+
+    /**
+     * lambda 默认方法
+     */
+    interface lambdaInterface {
+        String getMessage(String message);
+
+        default double pow(int num) {
+            return Math.pow(num,2);
+        }
+    }
+
+    @Test
+    public void test() {
+        lambdaInterface lambdaInterface = new lambdaInterface() {
+            @Override
+            public String getMessage(String message) {
+                return message;
+            }
+        };
+        System.out.println(lambdaInterface.getMessage("Hello"));
+        System.out.println(lambdaInterface.pow(50));
+    }
+
+}
